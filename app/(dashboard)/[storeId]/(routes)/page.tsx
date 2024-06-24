7506c2531dd3cf939c9d7937c4556193e03396c2
Luca Anequini Antoniazzi <lucanequini@gmail.com>
2024-06-24 00:33:20 -0300

    feat: build navbar content

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,8 +1,20 @@
-const DashboardPage = () => {
+import prismadb from "@/lib/prismadb"
+
+interface DashboardPageProps {
+    params: { storeId: string }
+}
+
+const DashboardPage: React.FC<DashboardPageProps> = async ({ params }) => {
+    const store = await prismadb.store.findFirst({
+        where: {
+            id: params.storeId
+        }
+    })
+
     return (
         <div>
-            This is a dashboard page!
-        </div>
+            Activate store: {store?.name}
+        </div >
     )
 }
 
