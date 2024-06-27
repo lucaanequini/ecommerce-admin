39bea1fa8c551a6836ad68a7360ccbb487092c0f
Luca Anequini Antoniazzi <lucanequini@gmail.com>
2024-06-27 00:47:09 -0300

    feat: create orders page and build its components

---
+++ app/(dashboard)/[storeId]/(routes)/orders/_components/client.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import { OrderColumn, columns } from "./columns"
+
+import { Heading } from "@/components/ui/heading"
+import { Separator } from "@/components/ui/separator"
+import { DataTable } from "@/components/ui/data-table"
+
+interface OrderProps {
+    data: OrderColumn[]
+}
+
+export const OrderClient: React.FC<OrderProps> = ({
+    data
+}) => {
+    return (
+        <>
+            <Heading
+                title={`Orders (${data.length})`}
+                description="Manage orders for your store."
+            />
+            <Separator />
+            <DataTable columns={columns} data={data} searchKey="label" />
+        </>
+    )
+}
